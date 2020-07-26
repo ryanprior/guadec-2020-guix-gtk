60fb322ed05287bdd89d01f29582ca32eb3f09f3
Ryan Prior <ryanprior@hey.com>
2020-07-25 19:31:49 -0500

    Copies pantheon-calculator definition into planner.scm

---
+++ planner.scm
@@ -0,0 +1,40 @@
+(define-public pantheon-calculator
+  (package
+    (name "pantheon-calculator")
+    (version "1.5.5")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/elementary/calculator")
+             (commit version)))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32
+         "1csxsr2c8qvl97xz9ahwn91z095nzgr0i1mbcb1spljll2sr9lkj"))))
+    (build-system meson-build-system)
+    (arguments
+     `(#:glib-or-gtk? #t
+       #:phases
+       (modify-phases %standard-phases
+         (add-after 'unpack 'disable-schema-cache-generation
+           (lambda _
+             (setenv "DESTDIR" "/")
+             #t)))))
+    (inputs
+     `(("granite" ,granite)
+       ("glib" ,glib)
+       ("gtk" ,gtk+)
+       ("libgee" ,libgee)))
+    (native-inputs
+     `(("cmake" ,cmake)
+       ("glib:bin" ,glib "bin") ; for glib-compile-schemas
+       ("gettext" ,gettext-minimal)
+       ("pkg-config" ,pkg-config)
+       ("vala" ,vala)))
+    (home-page "https://github.com/elementary/calculator")
+    (synopsis "Desktop calculator")
+    (description "Calculator is an application for performing simple
+arithmetic.  It is the default calculator application in the Pantheon
+desktop.")
+    (license license:gpl3)))
